c24e24e7b74f2bae1a07be62f13d9da140e36f50
SAndrian <salamahaandrian@dspu.edu.ua>
2024-12-07 02:19:06 +0200

    Lab_6.4r

--- Lab_6.4r/Lab_6.4.cpp
+++ Lab_6.4r/Lab_6.4.cpp
@@ -1,89 +1,113 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-void PrintArray(const vector<int>& a, size_t i = 0) {
-    if (i < a.size()) {
+void InputArray(int* a, int size, int i = 0) {
+    if (i < size) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+        InputArray(a, size, i + 1);
+    }
+}
+
+void PrintArray(const int* a, int size, int i = 0) {
+    if (i < size) {
         cout << "a[" << i << "] = " << a[i] << "; ";
-        PrintArray(a, i + 1);
-    } else {
+        PrintArray(a, size, i + 1);
+    }
+    else {
         cout << endl;
     }
 }
 
-int GetProductOfEvenIndex(const vector<int>& a, size_t i = 0) {
-    if (i >= a.size()) return 1; 
-    if (i % 2 == 0) {
-        return a[i] * GetProductOfEvenIndex(a, i + 2);
-    } else {
-        return GetProductOfEvenIndex(a, i + 1); 
-    }
+int GetProductOfEvenIndex(const int* a, int size, int i = 0) {
+    if (i >= size) return 1;
+    return (i % 2 == 0 ? a[i] : 1) * GetProductOfEvenIndex(a, size, i + 1);
 }
 
-int GetSumBetweenZeros(const vector<int>& a, size_t i = 0, int firstZero = -1, int lastZero = -1) {
-    if (i >= a.size()) {
-        if (firstZero == -1 || lastZero == -1 || firstZero == lastZero) {
-            return 0; 
-        }
-        int sum = 0;
-        for (int j = firstZero + 1; j < lastZero; ++j) {
-            sum += a[j];
-        }
-        return sum;
+int SumBetweenIndices(const int* a, int firstZero, int lastZero, int i) {
+    if (i >= lastZero) return 0;
+    return a[i] + SumBetweenIndices(a, firstZero, lastZero, i + 1);
+}
+
+int GetSumBetweenZeros(const int* a, int size, int i = 0, int firstZero = -1, int lastZero = -1) {
+    if (i >= size) {
+        if (firstZero == -1 || lastZero == -1 || firstZero == lastZero) return 0;
+        return SumBetweenIndices(a, firstZero, lastZero, firstZero + 1);
     }
 
     if (a[i] == 0) {
-        if (firstZero == -1) {
-            firstZero = i; 
-        }
-        lastZero = i; 
+        if (firstZero == -1) firstZero = i;
+        lastZero = i;
     }
 
-    return GetSumBetweenZeros(a, i + 1, firstZero, lastZero);
+    return GetSumBetweenZeros(a, size, i + 1, firstZero, lastZero);
 }
 
-void TransformArray(vector<int>& a, vector<int>& positives, vector<int>& negatives, size_t i = 0) {
-    if (i < a.size()) {
-        if (a[i] >= 0) {
-            positives.push_back(a[i]); 
-        } else {
-            negatives.push_back(a[i]); 
-        }
-        TransformArray(a, positives, negatives, i + 1); 
-    } else {
-        a = positives; 
-        a.insert(a.end(), negatives.begin(), negatives.end()); 
+void TransformArrayHelper(const int* a, int* positives, int* negatives, int size, int i, int& posCount, int& negCount) {
+    if (i >= size) return;
+
+    if (a[i] >= 0) {
+        positives[posCount++] = a[i];
+    }
+    else {
+        negatives[negCount++] = a[i];
+    }
+
+    TransformArrayHelper(a, positives, negatives, size, i + 1, posCount, negCount);
+}
+
+void TransformArray(int* a, int size, int*& positives, int& posCount, int*& negatives, int& negCount) {
+    positives = new int[size];
+    negatives = new int[size];
+    posCount = negCount = 0;
+
+    TransformArrayHelper(a, positives, negatives, size, 0, posCount, negCount);
+
+    int index = 0;
+    for (int i = 0; i < posCount; ++i) {
+        a[index++] = positives[i];
+    }
+    for (int i = 0; i < negCount; ++i) {
+        a[index++] = negatives[i];
     }
 }
 
 int main() {
-    int n;
+    int n; 
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    vector<int> a(n);
-    cout << "Enter the elements of the array:" << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
+    if (n <= 0) {
+        cout << "Invalid size!" << endl; 
+        return 1;
     }
 
+    int* a = new int[n]; 
+    cout << "Enter the elements of the array:" << endl;
+    InputArray(a, n);
+
     cout << "\nOriginal array:" << endl;
-    PrintArray(a);
+    PrintArray(a, n);
 
     // 1.1
-    int product = GetProductOfEvenIndex(a);
+    int product = GetProductOfEvenIndex(a, n);
     cout << "\n1.1. Product of elements with even indices: " << product << endl;
 
     // 1.2
-    int sum = GetSumBetweenZeros(a);
+    int sum = GetSumBetweenZeros(a, n);
     cout << "1.2. Sum of elements between first and last zero: " << sum << endl;
 
     // 2
-    vector<int> positives, negatives;
-    TransformArray(a, positives, negatives);
+    int* positives, * negatives;
+    int posCount, negCount;
+    TransformArray(a, n, positives, posCount, negatives, negCount);
     cout << "\n2. Transformed array:" << endl;
-    PrintArray(a);
+    PrintArray(a, n);
+
+    delete[] a;
+    delete[] positives;
+    delete[] negatives;
 
     return 0;
 }
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,36 +1,49 @@
-#include "pch.h"
+#include <sstream>
+#include <string>
+#include <iostream>
 #include "CppUnitTest.h"
 #include "../Lab_6.4r/Lab_6.4.cpp"
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTest1
-{
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
-        TEST_METHOD(TestCreateArrayRecursive)
+        using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+        void PrintArray(const int* a, int size, int i = 0);
+
+        TEST_CLASS(UnitTest)
         {
-            vector<int> a = { 1, -2, 3, 4, 5 };
-            stringstream ss;
-            for (size_t i = 0; i < a.size(); ++i) {
-                ss << "a[" << i << "] = " << a[i] << "; ";
+        public:
+            TEST_METHOD(TestCreateArrayRecursive)
+            {
+                const int size = 5;
+                int a[size] = { 1, -2, 3, 4, 5 };
+                std::stringstream ss;
+
+                for (int i = 0; i < size; ++i) {
+                    ss << "a[" << i << "] = " << a[i] << "; ";
+                }
+
+                std::ostringstream result;
+                std::streambuf* original = std::cout.rdbuf(result.rdbuf());
+
+                PrintArray(a, size);
+
+                std::cout.rdbuf(original);
+
+                std::string expected = ss.str();
+                std::string actual = result.str();
+
+                expected.erase(expected.find_last_not_of(" \n\r\t") + 1);
+                actual.erase(actual.find_last_not_of(" \n\r\t") + 1);
+
+                Assert::AreEqual(expected.c_str(), actual.c_str());
             }
+        };
 
-            ostringstream result;
-            std::streambuf* original = std::cout.rdbuf(result.rdbuf());
-
-            PrintArray(a); 
-
-            std::cout.rdbuf(original); 
-
-            string expected = ss.str();
-            string actual = result.str();
-
-            expected.erase(expected.find_last_not_of(" \n\r\t") + 1);
-            actual.erase(actual.find_last_not_of(" \n\r\t") + 1);
-
-            Assert::AreEqual(expected.c_str(), actual.c_str());
+        void PrintArray(const int* a, int size, int i) {
+            if (i < size) {
+                std::cout << "a[" << i << "] = " << a[i] << "; ";
+                PrintArray(a, size, i + 1);
+            }
+            else {
+                std::cout << std::endl;
+            }
         }
-	};
-}
